cd32149346a9380e1ffdbcb2c67336adc9980e2c
Lordseriouspig <lordseriouspig@gmail.com>
2026-04-15 08:04:47 +0000

    feat(tui): few things in tui

--- backend/src/tui/views/home.rs
+++ backend/src/tui/views/home.rs
@@ -22,7 +22,7 @@ use ratatui::{
     buffer::Buffer,
     layout::Rect,
     prelude::*,
-    widgets::{Block, Paragraph, Widget},
+    widgets::{Block, Paragraph, Widget, Wrap},
 };
 
 const ASCII_ART_1: &str = r#" _    _      _ _       _    _____ _
@@ -37,6 +37,8 @@ const ASCII_ART_2: &str = r#"██╗      ██████╗ ████�
 ██║     ██║   ██║██╔══██╗██║  ██║╚════██║██╔══╝  ██╔══██╗██║██║   ██║██║   ██║╚════██║██╔═══╝ ██║██║   ██║
 ███████╗╚██████╔╝██║  ██║██████╔╝███████║███████╗██║  ██║██║╚██████╔╝╚██████╔╝███████║██║     ██║╚██████╔╝
 ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝ ╚═════╝  ╚═════╝ ╚══════╝╚═╝     ╚═╝ ╚═════╝"#;
+
+const BODY_TEXT: &str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed condimentum nulla bibendum enim fermentum, id viverra ligula posuere. Curabitur hendrerit eros sed finibus porttitor. Vestibulum nec sollicitudin turpis. Maecenas sed nisl turpis. Mauris porttitor nec nunc vitae commodo. Nunc eget nibh sit amet ipsum tempus maximus. Duis venenatis malesuada metus imperdiet sodales. Etiam eu facilisis lectus. Nulla dui nisi, lobortis quis fermentum non, ultricies nec diam. Sed tristique est et augue viverra, id porta ex dignissim. Curabitur finibus nisl id sem suscipit, ut rhoncus enim aliquam. Phasellus ac lorem sit amet massa bibendum posuere. In convallis efficitur cursus. Etiam justo sapien, bibendum ac arcu ut, accumsan mattis ipsum. Vestibulum a venenatis elit.";
 
 pub fn render(border: Block<'static>, area: Rect, buf: &mut Buffer) {
     let border = border.padding(ratatui::widgets::Padding::horizontal(1));
@@ -53,17 +55,29 @@ pub fn render(border: Block<'static>, area: Rect, buf: &mut Buffer) {
             Constraint::Length(7),
             Constraint::Length(7),
             Constraint::Fill(1),
+            Constraint::Fill(2)
         ])
         .split(layout[0]);
     Paragraph::new(ASCII_ART_1)
         .block(block.clone())
         .render(inner[0], buf);
     Paragraph::new(ASCII_ART_2)
-        .block(block)
+        .block(block.clone())
         .style(Style::new().fg(Color::Rgb(
             themes::catppuccin::HEADING_TEXT.rgb.r,
             themes::catppuccin::HEADING_TEXT.rgb.g,
             themes::catppuccin::HEADING_TEXT.rgb.b,
         )))
         .render(inner[1], buf);
+    Paragraph::new(BODY_TEXT)
+        .block(block)
+        .style(Style::new().fg(Color::Rgb(
+            themes::catppuccin::BODY_TEXT.rgb.r,
+            themes::catppuccin::BODY_TEXT.rgb.g,
+            themes::catppuccin::BODY_TEXT.rgb.b,
+        )))
+        .wrap(Wrap { trim: true })
+        .render(inner[2], buf);
+    // TODO: Hackatime stats w/ loading anim
+    // TODO: Right column w/ links, stats, github stuff, slack api stuff, time, etc
 }

7e99ecf1f9a66be5b35b1128c73ae1437acc5a00
Lordseriouspig <lordseriouspig@gmail.com>
2026-06-30 15:58:57 +1000

    chore: remove redundant log

--- backend/src/app/models/sessions/session.rs
+++ backend/src/app/models/sessions/session.rs
@@ -36,7 +36,6 @@ pub struct SessionHandle {
 
 impl Session {
     pub fn new(id: Uuid) -> (Self, SessionHandle) {
-        tracing::info!("Created new session");
         let (output_tx, _) = broadcast::channel(100);
         let (input_tx, input_rx) = mpsc::unbounded_channel();
 
